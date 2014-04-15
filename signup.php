72d2d2037f443394754b93a1c95ffa9a7aa8c986
Jason Swierk <jason.swierk@flite.com>
2014-04-14 17:40:03 -0700

    writing to local file instead of signups.io

--- signup.php
+++ signup.php
@@ -1,8 +1,11 @@
 <?php
 $email = $_POST['username'];
+$check1 = $_POST['landlord'];
+//$check2 = $_POST['tenant'];
 
 //the data
-$data = "$email\n";
+//$data = "$email|$check1|$check2\n";
+$data = "$email|$check1\n";
 
 //open the file and choose the mode
 $fh = fopen("users.txt", "a");
