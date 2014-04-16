bd26bfed03235d0579f88021ad514541eb9efcef
Jason Swierk <jason.swierk@flite.com>
2014-04-15 20:57:29 -0700

    now, with comments

--- signup.php
+++ signup.php
@@ -1,11 +1,12 @@
 <?php
 $email = $_POST['username'];
-$check1 = $_POST['landlord'];
-$check2 = $_POST['tenant'];
+$comments = $_POST['comments'];
+
+if (isset($_POST['landlord'])) { $check1 = $_POST['landlord']; } else { $check1 = ""; }
+if (isset($_POST['tenant'])) { $check2 = $_POST['tenant']; } else { $check2 = ""; }
 
 //the data
-$data = "$email|$check1|$check2\n";
-//$data = "$email|$check1\n";
+$data = "$email|$check1|$check2|$comments\n";
 
 //open the file and choose the mode
 $fh = fopen("users.txt", "a");
